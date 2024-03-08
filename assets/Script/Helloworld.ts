f84e7471a7f0b307e5bd39279a7428b079004d80
DheerajMac <>
2024-03-08 15:24:31 +0530

    removed crowdin model

--- assets/Script/Helloworld.ts
+++ assets/Script/Helloworld.ts
@@ -1,4 +1,3 @@
-import { CrowdinLangManifest } from "./CrowdinModels";
 
 const {ccclass, property} = cc._decorator;
 
@@ -14,23 +13,6 @@ export default class Helloworld extends cc.Component {
     start () {
         // init logic
         this.label.string = this.text;
-        const data = this.getData("https://distributions.crowdin.net/fb8fb0cf928df2131f4bfa9n010/content/fr-CA/stringEN.json?timestamp=1709654183").then(respon => {
-            console.log(respon);
-        });
         
-    }
-    
-
-    async getData<T>(url:string):Promise<T>{
-        try {
-            const response = await fetch(url);
-            if (!response.ok) {
-                throw new Error('Network response was not ok.');
-            }
-            const data = await response.json();
-            return data
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
     }
 }
